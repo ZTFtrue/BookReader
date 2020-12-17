cb3dd3f0f4c98abdb7e3264e7ce92c78bee17a28
ztftrue <ztftrue@gmail.com>
2020-12-17 23:58:07 +0800

    ng update @angular/cli

--- src/app/dialog-settings/dialog-settings.component.spec.ts
+++ src/app/dialog-settings/dialog-settings.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { DialogSettingsComponent } from './dialog-settings.component';
 
@@ -6,7 +6,7 @@ describe('DialogNavgationComponent', () => {
   let component: DialogSettingsComponent;
   let fixture: ComponentFixture<DialogSettingsComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ DialogSettingsComponent ]
     })
